54b23b1eb453f4a5468a3373c30423cd369b0570
JoaoOliveiraZz <joaolima.gxp@gmail.com>
2025-02-27 20:14:15 -0300

    [FIX] Returning the operator to stdin when the number is completed.

--- analex.h
+++ analex.h
@@ -1,23 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <ctype.h>
 
 #define NUM 256
 
 int tokenVal;
 
-int isDigit(char digit){
-
-    if(digit > 48 && digit < 58){
-        return 1;
-    }
-
-    return 0;
-
-}
-
 int analex(){
-
     char ch = getchar();
+
+    while(ch == ' ' || ch == '\n' || ch == '\t') ch = getchar();
 
     if(
         ch == '+' ||
@@ -29,8 +21,18 @@ int analex(){
         ch == ';'
     ) return ch;
 
-    if(isDigit(ch)) return NUM;
+    if(isdigit(ch)){
+        tokenVal = ch - '0';
+        while(isdigit(ch)){
+            tokenVal = tokenVal * 10 + (ch - '0');
+            ch = getchar();
+        }
 
-    printf("Lexical Error, invalid token.\n");
+        ungetc(ch, stdin);
 
+        return NUM;
+    }
+
+    printf("Lexical Error.\n");
+    exit(1);
 }
